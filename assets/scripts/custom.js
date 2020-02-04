cd7ffd5161c00aa809ba6ad41e861d0e54c7e0db
HiagoRaD <58034997+HiagoRaD@users.noreply.github.com>
2020-02-04 12:45:09 -0300

    Added the home page and first draft of "about me"

---
+++ assets/scripts/custom.js
@@ -0,0 +1,37 @@
+var _PALHETA = '.color-ff5454';
+
+$('i').addClass(_PALHETA);
+
+$(document).ready(function () {
+    // Side menu animations - DESKTOP
+    $('.head-wrapper').mouseenter(function () {
+        $(this).css('width', '210px');
+        $('.info-tab').show();
+        $('.info-tab').addClass('animated fadeIn delay-1');
+        $('.logo-div').css('color', '#ff5454');
+        $('.sub-logo').show();
+        $('.sub-logo').addClass('animated fadeIn delay-2');
+        $('.icon-logo').css('color', '#ff5454');
+        $('.icon-logo').addClass('animated fadeIn');
+        $('.icon-bar').css('color', '#ff5454');
+    }).mouseleave(function () {
+        $(this).css('width', '70px');
+        $('.info-tab').hide();
+        $('.sub-logo').hide();
+        $('.logo-div').css("color", '#ffffff');
+        $('.icon-logo').css('color', '#ffffff');
+        $('.icon-bar').css('color', '#ffffff');
+    });
+
+
+    // btn go to 
+    $('.goto').click(function() {
+        var current = $(this).attr('id').split('_')[0];
+        var next = $(this).attr('id').split('_')[2];
+
+        $('.home').addClass('animated fadeOutDown');
+        
+        $('.about-me').show();
+        $('.about-me').addClass('animated fadeInDown');
+    });
+});
